e388408625b21249e12401e847792f6c162a5cff
Vijay Agrawal <vijay95051@gmail.com>
2017-01-25 12:48:06 +0530

    check if file exists

--- before-install.bat
+++ before-install.bat
@@ -1,4 +1,5 @@
 REM Install Internet Information Server (IIS). 
 #c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
 #c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
+rmdir /S /Q D:\inetpub\www
 mkdir D:\inetpub\www
